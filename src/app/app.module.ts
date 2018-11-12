f15b8e84166a747d2156feee9a5bbc6907aa52e1
cuongdq.payment <cuongdq.payment@gmail.com>
2018-11-12 23:34:37 +0700

    HEROKU Deployment by cuongdq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-import { WeatherProvider } from '../providers/weather/weather';
-import { HttpModule} from '@angular/http';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    WeatherProvider,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+ declarations: [
+   MyApp,
+   HomePage
+ ],
+ imports: [
+   BrowserModule,
+   IonicModule.forRoot(MyApp),
+   HttpClientModule
+ ],
+ bootstrap: [IonicApp],
+ entryComponents: [
+   MyApp,
+   HomePage
+ ],
+ providers: [
+   StatusBar,
+   SplashScreen,
+   {provide: ErrorHandler, useClass: IonicErrorHandler}
+ ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { WeatherProvider} from "../../providers/weather/weather"
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map";
+import { Platform } from 'ionic-angular/platform/platform';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+ selector: 'page-home',
+ templateUrl: 'home.html'
 })
 export class HomePage {
 
-  weather:any;
-  location:{
-    city:string,
-    state:string
-  }
+ users: Observable<any>;
 
-  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+ constructor(public navCtrl: NavController, private httpClient: HttpClient, private plt: Platform, private alertCtrl: AlertController) {
+   this.users = this.httpClient.get('https://randomuser.me/api/?results=20')
+   .map(res => res['results'])
+ }
 
-  }
+ checkPlatform() {
+   let alert = this.alertCtrl.create({
+     title: 'Platform',
+     message: 'You are running on: ' + this.plt.platforms(),
+     buttons: ['OK']
+   });
+   alert.present();
 
-  ionViewWillEnter(){
-    this.location = {
-      city:"Miami",
-      state: "FL"
-    }
-
-    this.weatherProvider.getWeather(this.location.city,this.location.state)
-    .subscribe(weather => {
-      //console.log(weather);
-      this.weather = weather;
-    })
-  }
-
+   if (this.plt.is('cordova')) {
+     // Do Cordova stuff
+   } else {
+     // Do stuff inside the regular browser
+   }
+ }
 }
