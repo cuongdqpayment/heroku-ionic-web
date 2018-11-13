f0efd1b0f15112f66da2ef58ef8ef9760f6c78d4
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2018-11-13 13:38:39 +0700

    cuongdq push by sh

--- heroku-push.sh
+++ heroku-push.sh
@@ -5,5 +5,7 @@ ionic cordova build browser --prod --release
 
 git add .
 git commit -am 'HEROKU Deployment by cuongdq'
+#git remote rm heroku
+#heroku git:remote -a cuongdq-ionic
 git push heroku master
 heroku open
